fec46626f7cd472bf1b6a0edea4a7b9f518f10aa
nilsroesel <nils.roesel@yahoo.de>
2018-05-15 22:12:18 +0200

    Add new decorators to index file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export * from './decorators/accessors/getter.decorator';
 export * from './decorators/accessors/setter.decorator';
 export * from './decorators/configger.decorator';
+export * from './decorators/class/accessors.decorator';
+export * from './decorators/class/builder.decorator';
+export * from './decorators/class/define-property.decorator';
+export * from './decorators/class/to-string.decorator';
+export * from './decorators/class/data.decorator';
+
 export * from './utils/index';
